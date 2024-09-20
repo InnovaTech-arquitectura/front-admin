28a2e76f9539ab5b44c20af5ff7aa33b70f41f59
RhaxCity <juanjogomezarenas1@gmail.com>
2024-09-20 10:12:43 -0500

    Corregido para que pase las pruebas

--- src/app/publicidad/ver-banners/ver-banners.component.spec.ts
+++ src/app/publicidad/ver-banners/ver-banners.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VerBannersComponent } from './ver-banners.component';
+import { HeaderComponent } from 'src/app/componentTools/header/header.component';
+import { SidebarComponent } from 'src/app/componentTools/sidebar/sidebar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('VerBannersComponent', () => {
   let component: VerBannersComponent;
@@ -8,7 +11,8 @@ describe('VerBannersComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [VerBannersComponent]
+      declarations: [VerBannersComponent, HeaderComponent, SidebarComponent],
+      imports:[RouterTestingModule]
     });
     fixture = TestBed.createComponent(VerBannersComponent);
     component = fixture.componentInstance;
