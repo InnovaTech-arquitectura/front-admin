fd3cecfd52173b7e2d1f7d4e4ba3b2f61145059a
RhaxCity <juanjogomezarenas1@gmail.com>
2024-09-21 08:31:21 -0500

    Realizadas pantallas de publicidad y pruebas de las mismas

--- src/app/publicidad/nuevo-banner/nuevo-banner.component.spec.ts
+++ src/app/publicidad/nuevo-banner/nuevo-banner.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NuevoBannerComponent } from './nuevo-banner.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HeaderComponent } from 'src/app/componentTools/header/header.component';
+import { SidebarComponent } from 'src/app/componentTools/sidebar/sidebar.component';
 
 describe('NuevoBannerComponent', () => {
   let component: NuevoBannerComponent;
@@ -8,7 +11,8 @@ describe('NuevoBannerComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NuevoBannerComponent]
+      declarations: [NuevoBannerComponent, HeaderComponent, SidebarComponent],
+      imports:[RouterTestingModule]
     });
     fixture = TestBed.createComponent(NuevoBannerComponent);
     component = fixture.componentInstance;
