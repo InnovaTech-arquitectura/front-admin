29509e66aeae0a879c3365dc2fd2120332f903e0
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2024-09-23 23:28:41 -0500

    Pruebas

--- src/app/perfiles/ver-perfiles/ver-perfiles.component.spec.ts
+++ src/app/perfiles/ver-perfiles/ver-perfiles.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';  // Import RouterTestingModule
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Importar HttpClientTestingModule
 import { VerPerfilesComponent } from './ver-perfiles.component';
+import { PerfilesService } from 'src/app/service/perfiles.service'; // Asegúrate de la ruta correcta de PerfilesService
 
 describe('VerPerfilesComponent', () => {
   let component: VerPerfilesComponent;
@@ -11,8 +12,12 @@ describe('VerPerfilesComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [VerPerfilesComponent],
-      imports: [RouterTestingModule],  // Add RouterTestingModule here
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Add CUSTOM_ELEMENTS_SCHEMA here
+      imports: [
+        RouterTestingModule, // Importa RouterTestingModule
+        HttpClientTestingModule // Importa HttpClientTestingModule
+      ],
+      providers: [PerfilesService], // Proveer PerfilesService
+      schemas: [CUSTOM_ELEMENTS_SCHEMA] // Agrega CUSTOM_ELEMENTS_SCHEMA
     });
     fixture = TestBed.createComponent(VerPerfilesComponent);
     component = fixture.componentInstance;
