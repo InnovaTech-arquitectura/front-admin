1951269798392258c2c0d0a495d8733cf63285ba
Shermann103 <hhermann103@gmail.com>
2024-10-01 11:09:34 -0500

    Corrección prueba front

--- src/app/perfiles/perfil-nuevo-empleado/perfil-nuevo-empleado.component.spec.ts
+++ src/app/perfiles/perfil-nuevo-empleado/perfil-nuevo-empleado.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { PerfilNuevoEmpleadoComponent } from './perfil-nuevo-empleado.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 describe('PerfilNuevoEmpleadoComponent', () => {
   let component: PerfilNuevoEmpleadoComponent;
@@ -10,7 +12,7 @@ describe('PerfilNuevoEmpleadoComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [PerfilNuevoEmpleadoComponent],
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientModule, FormsModule],  // Add RouterTestingModule here
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     fixture = TestBed.createComponent(PerfilNuevoEmpleadoComponent);
