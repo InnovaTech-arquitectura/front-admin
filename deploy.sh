52800b6df8f5bbf9e4406db5793744c346af038e
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-01 20:45:42 -0500

    update Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 
 echo "Iniciando proceso de despliegue..."
 
-# Cambia al directorio donde está el script y los archivos
-cd "$(dirname "$0")" || exit 1  # Esto te llevará al directorio donde se encuentra deploy.sh
-
 echo "Deteniendo y eliminando contenedor existente..."
 docker stop front-admin-container || true
 docker rm front-admin-container || true
