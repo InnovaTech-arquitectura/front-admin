0eb4a63614f1ac536a229d490ce4ffda0fbd6ff8
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-01 20:50:16 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,24 @@
 
 echo "Iniciando proceso de despliegue..."
 
+# Cambiar al directorio donde se va a copiar el repositorio
+DEST_DIR="/home/estudiante/Desktop/front-admin"
+
 echo "Deteniendo y eliminando contenedor existente..."
 docker stop front-admin-container || true
 docker rm front-admin-container || true
+
+# Eliminar el directorio existente, si existe
+rm -rf "$DEST_DIR"
+
+# Crear un nuevo directorio para el despliegue
+mkdir -p "$DEST_DIR"
+
+# Copiar los archivos del repositorio al servidor
+scp -r * estudiante@10.43.101.107:"$DEST_DIR"
+
+# Cambiar al directorio de despliegue
+cd "$DEST_DIR" || exit 1  # Esto te llevará al directorio donde se encuentra docker-compose.yml
 
 echo "Construyendo y ejecutando los contenedores Docker..."
 docker-compose up --build -d
