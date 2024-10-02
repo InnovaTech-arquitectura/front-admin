fa2e0ccbaa361f226b2da3c86b36a8d28a2ae18d
Andrés García Montoya <61592522+andres345gm@users.noreply.github.com>
2024-10-02 07:53:14 -0500

    Update environment.testing.ts

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseApiUrl: 'http://10.43.100.236', // Cambia esto a la URL base en el entorno de testing
+    baseApiUrl: 'http://10.43.100.236:8090', // Cambia esto a la URL base en el entorno de testing
   };
   
