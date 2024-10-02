4ef8e6fdfff81c42188f5f33b3c0894bbc975cd1
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-01 21:18:17 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,24 @@
 
 echo "Iniciando proceso de despliegue..."
 
-# Cambiar al directorio donde se va a copiar el repositorio
-DEST_DIR="/home/estudiante/Desktop/front-admin"
+# Cambiar al directorio donde se clonará el repositorio
+cd /home/estudiante/Desktop || exit 1
+
+# Clonar el repositorio (si ya existe, lo eliminamos primero)
+if [ -d "front-admin" ]; then
+    echo "Eliminando el directorio existente de front-admin..."
+    rm -rf front-admin
+fi
+
+echo "Clonando el repositorio..."
+git clone https://github.com/InnovaTech-arquitectura/front-admin.git
+
+# Cambiar al directorio del repositorio clonado
+cd front-admin || exit 1
 
 echo "Deteniendo y eliminando contenedor existente..."
 docker stop front-admin-container || true
 docker rm front-admin-container || true
-
-# Eliminar el directorio existente, si existe
-rm -rf "$DEST_DIR"
-
-# Crear un nuevo directorio para el despliegue
-mkdir -p "$DEST_DIR"
-
-# Copiar los archivos del repositorio al servidor
-scp -r * estudiante@10.43.101.107:"$DEST_DIR"
-
-# Cambiar al directorio de despliegue
-cd "$DEST_DIR" || exit 1  # Esto te llevará al directorio donde se encuentra docker-compose.yml
 
 echo "Construyendo y ejecutando los contenedores Docker..."
 docker-compose up --build -d
