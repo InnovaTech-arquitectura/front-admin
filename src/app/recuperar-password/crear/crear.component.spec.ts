a16ebb4ebf103d849c0e25470d1e0d501a19fc45
mcardonar <mp.cardonar@javeriana.edu.co>
2024-10-07 14:31:53 -0500

    cargando cambios

--- src/app/recuperar-password/crear/crear.component.spec.ts
+++ src/app/recuperar-password/crear/crear.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CrearComponent } from './crear.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Importamos HttpClientTestingModule para pruebas de HttpClient
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; // Añadimos CUSTOM_ELEMENTS_SCHEMA
+import { FormsModule } from '@angular/forms'; // Importamos FormsModule para el uso de ngModel
 
 describe('CrearComponent', () => {
   let component: CrearComponent;
@@ -8,7 +10,9 @@ describe('CrearComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CrearComponent]
+      declarations: [CrearComponent],
+      imports: [HttpClientTestingModule, FormsModule], // Importamos FormsModule para evitar el error de ngModel
+      schemas: [CUSTOM_ELEMENTS_SCHEMA] // Añadimos el esquema para evitar problemas con componentes personalizados
     });
     fixture = TestBed.createComponent(CrearComponent);
     component = fixture.componentInstance;
--- src/app/service/recuperar-password.service.spec.ts
+++ src/app/service/recuperar-password.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RecuperarPasswordService } from './recuperar-password.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; // Importa el schema
 
 describe('RecuperarPasswordService', () => {
   let service: RecuperarPasswordService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Importa el módulo de testing de HttpClient
+      providers: [RecuperarPasswordService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // Incluye el schema
+    });
     service = TestBed.inject(RecuperarPasswordService);
   });
 
