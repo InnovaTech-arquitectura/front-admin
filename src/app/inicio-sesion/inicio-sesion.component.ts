6f79e2ef2ad89e0811bd03d26c3c425b771d8bfc
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-10-07 19:58:39 -0500

    pqrs

--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -4,39 +4,39 @@ import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-inicio-sesion',
-  templateUrl: './inicio-sesion.component.html',
-  styleUrls: ['./inicio-sesion.component.css']
+	selector: 'app-inicio-sesion',
+	templateUrl: './inicio-sesion.component.html',
+	styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
-  email: string = '';
-  password: string = '';
-  isLoading: boolean = false; // Estado de carga
+	email: string = '';
+	password: string = '';
+	isLoading: boolean = false; // Estado de carga
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+	constructor(
+		private authService: AuthService,
+		private router: Router
+	) {}
 
-  onSubmit() {
-    this.isLoading = true; // Mostrar el spinner
-    this.authService.login(this.email, this.password).subscribe(
-      response => {
-        console.log('Inicio de sesión exitoso:', response);
-        localStorage.setItem('token', response);
-        this.router.navigate(['/publicidad']);
-        this.isLoading = false; // Ocultar el spinner
-      },
-      error => {
-        console.error('Error al iniciar sesión:', error);
-        this.isLoading = false; // Ocultar el spinner
-        Swal.fire({
-          icon: 'error',
-          title: 'Error de autenticación',
-          text: 'No pudimos iniciar sesión. Por favor, verifica tus credenciales.',
-          confirmButtonText: 'Cerrar'
-        });
-      }
-    );
-  }
+	onSubmit() {
+		this.isLoading = true; // Mostrar el spinner
+		this.authService.login(this.email, this.password).subscribe(
+			(response) => {
+				console.log('Inicio de sesión exitoso:', response);
+				localStorage.setItem('token', response);
+				this.router.navigate(['/publicidad']);
+				this.isLoading = false; // Ocultar el spinner
+			},
+			(error) => {
+				console.error('Error al iniciar sesión:', error);
+				this.isLoading = false; // Ocultar el spinner
+				Swal.fire({
+					icon: 'error',
+					title: 'Error de autenticación',
+					text: 'No pudimos iniciar sesión. Por favor, verifica tus credenciales.',
+					confirmButtonText: 'Cerrar'
+				});
+			}
+		);
+	}
 }
--- src/app/publicidad/ver-banners/ver-banners.component.ts
+++ src/app/publicidad/ver-banners/ver-banners.component.ts
@@ -2,52 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import { PublicidadService } from 'src/app/service/publicidad.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { Banner } from 'src/app/model/banner';
 
 @Component({
-  selector: 'app-ver-banners',
-  templateUrl: './ver-banners.component.html',
-  styleUrls: ['./ver-banners.component.css']
+	selector: 'app-ver-banners',
+	templateUrl: './ver-banners.component.html',
+	styleUrls: ['./ver-banners.component.css']
 })
 export class VerBannersComponent implements OnInit {
-  banners: any[] = [];
+	banners: Banner[] = [];
+	interactions: number = 1;
 
-  constructor(private publicidadService: PublicidadService, private router: Router) {}
+	constructor(
+		private publicidadService: PublicidadService,
+		private router: Router
+	) {}
 
-  ngOnInit(): void {
-    this.publicidadService.listBanners().subscribe(
-      (data) => {
-        this.banners = data;
-      },
-      (error) => {
-        Swal.fire('Error', 'Error al cargar los banners.', 'error');
-      }
-    );
-  }
+	ngOnInit(): void {
+		this.publicidadService.listBanners().subscribe(
+			(data) => {
+				this.banners = data;
+				console.log(this.banners);
+			},
+			(error) => {
+				Swal.fire('Error', 'Error al cargar los banners.', 'error');
+			}
+		);
+	}
 
-  editBanner(id: number): void {
-    this.router.navigate([`/editar-banner/${id}`]);
-  }
+	editBanner(id: number): void {
+		this.router.navigate([`/editar-banner/${id}`]);
+	}
 
-  deleteBanner(id: number): void {
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: "Esta acción no se puede deshacer.",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, eliminar',
-      cancelButtonText: 'No, cancelar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.publicidadService.deleteBanner(id).subscribe(
-          () => {
-            this.banners = this.banners.filter((banner) => banner.id !== id);
-            Swal.fire('Eliminado', 'El banner ha sido eliminado con éxito.', 'success');
-          },
-          (error) => {
-            Swal.fire('Error', 'Ocurrió un error al eliminar el banner.', 'error');
-          }
-        );
-      }
-    });
-  }
+	deleteBanner(id: number): void {
+		Swal.fire({
+			title: '¿Estás seguro?',
+			text: 'Esta acción no se puede deshacer.',
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Sí, eliminar',
+			cancelButtonText: 'No, cancelar'
+		}).then((result) => {
+			if (result.isConfirmed) {
+				this.publicidadService.deleteBanner(id).subscribe(
+					() => {
+						this.banners = this.banners.filter((banner) => banner.id !== id);
+						Swal.fire('Eliminado', 'El banner ha sido eliminado con éxito.', 'success');
+					},
+					(error) => {
+						Swal.fire('Error', 'Ocurrió un error al eliminar el banner.', 'error');
+					}
+				);
+			}
+		});
+	}
 }
